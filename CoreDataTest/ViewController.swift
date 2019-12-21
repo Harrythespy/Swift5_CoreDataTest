8bd59246502775b0ecb38fa9bb5b95d7266ee140
Harry Shen <tn850826@icloud.com>
2019-12-21 14:39:12 +0800

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Harry Shen on 21/12/19.
+//  Copyright © 2019 Harry Shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
